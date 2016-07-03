00c3de0487ee1f27044ce1fd9cde5ecd41cddf07
Curtis X <wudifeixue@users.noreply.github.com>
2016-07-03 06:17:49 -0600

    Update MinecraftServerAutoRestart.bat

--- windowsScripts/MinecraftServerAutoRestart.bat
+++ windowsScripts/MinecraftServerAutoRestart.bat
@@ -6,3 +6,16 @@ echo Server will restart in 10 seconds......
 timeout 10
 echo Restarting......
 goto Loop
+
+
+
+::while true
+::do
+::java -Xms2G -Xmx4G -jar server.jar nogui
+::echo "Server will restart in 10 seconds......"
+::sleep 10
+::echo "Restarting......"
+::done
+
+
+::bash for linux is very simliar so remove the comment to use on Linux
